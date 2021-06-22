2442ca04095642d5b337be27d9d69df5db19e7bd
cis-gaurav <gaurav.j@cisinlabs.com>
2021-06-22 19:09:38 +0530

    server

--- src/main/java/com/md/base/TestBase.java
+++ src/main/java/com/md/base/TestBase.java
@@ -43,7 +43,7 @@ public class TestBase extends ExtentReporterNG {
 	public static void initialization() throws MalformedURLException {					
 
 		//Define Execution Environment here i.e. Local/server
-		String ExecutionLocation = "local";
+		String ExecutionLocation = "server";
 
 		if (ExecutionLocation.equals("server")) {
 			 String chromedriverpath="",s;
