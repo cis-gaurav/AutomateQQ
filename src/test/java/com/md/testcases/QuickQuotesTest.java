0e96334649cf03ef9e4f2481679be743efa97e6c
cis-gaurav <gaurav.j@cisinlabs.com>
2021-10-14 20:21:10 +0530

    comment all code

--- src/test/java/com/md/testcases/QuickQuotesTest.java
+++ src/test/java/com/md/testcases/QuickQuotesTest.java
@@ -26,16 +26,16 @@ public class QuickQuotesTest extends TestBase {
 		QuickQuotesPage.redirectFromMDDashboardtoQQCreate();
 	}
 
-//	@Test(priority=1)
-//	public void qq_VerifyValidationMessageOnNamefield() throws InterruptedException {
-//		boolean flag = QuickQuotesPage.createNameFieldValidaions();
-//		Assert.assertTrue(flag);
-//	}
+	@Test(priority=1)
+	public void qq_VerifyValidationMessageOnNamefield() throws InterruptedException {
+		boolean flag = QuickQuotesPage.createNameFieldValidaions();
+		Assert.assertTrue(flag);
+	}
 	
 	@Test(priority=2)
 	public void qq_VerifyGetQuoteButtonIsDisable () throws InterruptedException {
 		boolean flag = QuickQuotesPage.createGetQuoteButtonIsDisable();
-		Assert.assertFalse(flag);
+		Assert.assertTrue(flag);
 	}
 	
 	@Test(priority=3)
@@ -50,27 +50,27 @@ public class QuickQuotesTest extends TestBase {
 		boolean flag = QuickQuotesPage.createQQByName();
 		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
 		System.out.println("QQ  @test2");
-		Assert.assertFalse(flag);
+		Assert.assertTrue(flag);
 	}
 
-//	@Test (priority=6)
-//	public void qq_VerfiyQQEventWithAllDetails() throws InterruptedException {
-//		boolean flag=QuickQuotesPage.createQQEventWithAllDetails();
-//		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
-//	}
-//
-//	@Test(priority=7)	
-//	public void qq_VerifySandpitEventWithQuestionnaireAndAllowResubmission() throws InterruptedException {
-//		boolean flag=QuickQuotesPage.createSandpitEventWithQuestionnaireAndAllowResubmission();
-//		Assert.assertTrue(flag,"verifySandpitEventWithQuestionnaireAndAllowResubmission is failed ask devloper to fix ");
-//	}
-//	
-//	@Test(priority=8)	
-//	public void qq_VerifyEditContent() throws InterruptedException { 
-//		boolean flag= QuickQuotesPage.createEditContent();
-//		Assert.assertTrue(flag);
-//	}
-//
+	@Test (priority=6)
+	public void qq_VerfiyQQEventWithAllDetails() throws InterruptedException {
+		boolean flag=QuickQuotesPage.createQQEventWithAllDetails();
+		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
+	}
+
+	@Test(priority=7)	
+	public void qq_VerifySandpitEventWithQuestionnaireAndAllowResubmission() throws InterruptedException {
+		boolean flag=QuickQuotesPage.createSandpitEventWithQuestionnaireAndAllowResubmission();
+		Assert.assertTrue(flag,"verifySandpitEventWithQuestionnaireAndAllowResubmission is failed ask devloper to fix ");
+	}
+	
+	@Test(priority=8)	
+	public void qq_VerifyEditContent() throws InterruptedException { 
+		boolean flag= QuickQuotesPage.createEditContent();
+		Assert.assertTrue(flag);
+	}
+
 //	@Test(priority=8)
 //	public void Rough() throws InterruptedException {
 //		QuickQuotesPage.Rough1();
