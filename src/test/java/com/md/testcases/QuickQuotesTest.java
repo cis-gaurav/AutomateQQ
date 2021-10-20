807d944dd8c63b1c2de8f3a9a27ef8e2af8d03a2
cis-gaurav <gaurav.j@cisinlabs.com>
2021-10-20 14:15:54 +0530

    add 1 more test

--- src/test/java/com/md/testcases/QuickQuotesTest.java
+++ src/test/java/com/md/testcases/QuickQuotesTest.java
@@ -38,13 +38,13 @@ public class QuickQuotesTest extends TestBase {
 //		Assert.assertFalse(flag);
 		Assert.assertTrue(flag);
 	}
-//	
-//	@Test(priority=3)
-//	public void qq_VerifyToasterMessage() throws InterruptedException {
-//		boolean flag = QuickQuotesPage.createToasterMessage();
-//		Assert.assertTrue(flag, "verifyToasterMessage is failed ask developer to fix");
-//	}
-//
+	
+	@Test(priority=3)
+	public void qq_VerifyToasterMessage() throws InterruptedException {
+		boolean flag = QuickQuotesPage.createToasterMessage();
+		Assert.assertTrue(flag, "verifyToasterMessage is failed ask developer to fix");
+	}
+
 //	@Test(priority=4)
 //	public void qq_VerifyQuotebyName() throws Throwable {
 //		boolean flag = QuickQuotesPage.createQQByName();
