c664595b6f99f2471c6647091c0e0e7d7a17ddcd
cis-gaurav <gaurav.j@cisinlabs.com>
2021-10-29 17:37:54 +0530

    changes

--- src/test/java/com/md/testcases/QuickQuotesTest.java
+++ src/test/java/com/md/testcases/QuickQuotesTest.java
@@ -27,65 +27,65 @@ public class QuickQuotesTest extends TestBase {
 	}
 
 	@Test(priority=1)
-	public void qq_VerifyValidationMessageOnNamefield() throws InterruptedException {
-		boolean flag = QuickQuotesPage.createNameFieldValidaions();
-		Assert.assertTrue(flag);
-	}
-	
-	@Test(priority=2)
 	public void qq_VerifyGetQuoteButtonIsDisable () throws InterruptedException {
 		boolean flag = QuickQuotesPage.createGetQuoteButtonIsDisable();
 		Assert.assertFalse(flag);
 	}
 	
-	@Test(priority=3)
-	public void qq_VerifyToasterMessage() throws InterruptedException {
-		boolean flag = QuickQuotesPage.createToasterMessage();
-		Assert.assertTrue(flag, "verifyToasterMessage is failed ask developer to fix");
-	}
-
-	@Test(priority=4)
-	public void qq_VerifyQuotebyName() throws Throwable {
-		boolean flag = QuickQuotesPage.createQQByName();
-		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
-	}
-
-	@Test (priority=5)
-	public void qq_VerfiyQQEventWithAllDetails() throws InterruptedException {
-		boolean flag=QuickQuotesPage.createQQEventWithAllDetails();
-		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
-	}
-
-	@Test(priority=6)	
-	public void qq_VerifySandpitEventWithQuestionnaireAndAllowResubmission() throws InterruptedException {
-		boolean flag=QuickQuotesPage.createSandpitEventWithQuestionnaireAndAllowResubmission();
-		Assert.assertTrue(flag,"verifySandpitEventWithQuestionnaireAndAllowResubmission is failed ask devloper to fix ");
-	}
-	
-	@Test(priority=7)	
-	public void qq_VerifyEditContent() throws InterruptedException { 
-		boolean flag= QuickQuotesPage.createEditContent();
-		Assert.assertTrue(flag);
-	}
-
-	@Test(priority=8)
-	public void qq_VerifyDeleteQuote() throws InterruptedException {
-		QuickQuotesPage.deleteQuote();
-	}
-   
-	@Test(priority=9)
-	public void qq_VerifySearchQuote() throws InterruptedException {
-		QuickQuotesPage.searchQuote();
-	}
-	
-	@Test(priority=10)
-	public void qq_VerifyCreateMessage() throws InterruptedException {
-		QuickQuotesPage.createMessage();
-	}
-   
-	@AfterClass
-	public void Teardown() throws Exception {
-		logger.info("*******QQ_test ends*******");
-		driver.close();
-	}
+//	@Test(priority=2)
+//	public void qq_VerifyValidationMessageOnNamefield() throws InterruptedException {
+//		boolean flag = QuickQuotesPage.createNameFieldValidaions();
+//		Assert.assertTrue(flag);
+//	}
+//	
+//	@Test(priority=3)
+//	public void qq_VerifyToasterMessage() throws InterruptedException {
+//		boolean flag = QuickQuotesPage.createToasterMessage();
+//		Assert.assertTrue(flag, "verifyToasterMessage is failed ask developer to fix");
+//	}
+//
+//	@Test(priority=4)
+//	public void qq_VerifyQuotebyName() throws Throwable {
+//		boolean flag = QuickQuotesPage.createQQByName();
+//		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
+//	}
+//
+//	@Test (priority=5)
+//	public void qq_VerfiyQQEventWithAllDetails() throws InterruptedException {
+//		boolean flag=QuickQuotesPage.createQQEventWithAllDetails();
+//		Assert.assertTrue(flag, "verfiyQQEventWithAllDetails is failed ask developer to fix");
+//	}
+//
+//	@Test(priority=6)	
+//	public void qq_VerifySandpitEventWithQuestionnaireAndAllowResubmission() throws InterruptedException {
+//		boolean flag=QuickQuotesPage.createSandpitEventWithQuestionnaireAndAllowResubmission();
+//		Assert.assertTrue(flag,"verifySandpitEventWithQuestionnaireAndAllowResubmission is failed ask devloper to fix ");
+//	}
+//	
+//	@Test(priority=7)	
+//	public void qq_VerifyEditContent() throws InterruptedException { 
+//		boolean flag= QuickQuotesPage.createEditContent();
+//		Assert.assertTrue(flag);
+//	}
+//
+//	@Test(priority=8)
+//	public void qq_VerifyDeleteQuote() throws InterruptedException {
+//		QuickQuotesPage.deleteQuote();
+//	}
+//   
+//	@Test(priority=9)
+//	public void qq_VerifySearchQuote() throws InterruptedException {
+//		QuickQuotesPage.searchQuote();
+//	}
+//	
+//	@Test(priority=10)
+//	public void qq_VerifyCreateMessage() throws InterruptedException {
+//		QuickQuotesPage.createMessage();
+//	}
+//   
+//	@AfterClass
+//	public void Teardown() throws Exception {
+//		logger.info("*******QQ_test ends*******");
+//		driver.close();
+//	}
 }
