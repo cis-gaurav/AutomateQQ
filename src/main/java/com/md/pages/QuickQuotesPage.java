d7a1bffdc3cbaae0925a7d28acdc25f1d56fccda
cis-gaurav <gaurav.j@cisinlabs.com>
2021-12-09 22:28:51 +0530

    tes

--- src/main/java/com/md/pages/QuickQuotesPage.java
+++ src/main/java/com/md/pages/QuickQuotesPage.java
@@ -491,9 +491,9 @@ public class QuickQuotesPage extends TestBase {
 	}
 
 	public boolean createQQByName() throws Throwable {
-		Thread.sleep(4000);
+//		Thread.sleep(4000);
 		driver.navigate().to(prop.getProperty("QuickQuotesHomepage"));
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver, 60);
 		wait.until(ExpectedConditions.visibilityOf(name));
 		wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(whoToInviteText)));
