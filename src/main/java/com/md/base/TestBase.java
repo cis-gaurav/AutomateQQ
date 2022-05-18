e7ea2e7f1d416064a2da474d3e5f9396c2c79a2e
cis-gaurav <gaurav.j@cisinlabs.com>
2022-05-18 13:27:14 +0530

    corrct wait

--- src/main/java/com/md/base/TestBase.java
+++ src/main/java/com/md/base/TestBase.java
@@ -26,7 +26,7 @@ public class TestBase {
 
 	public static WebDriver driver;
 	public static Properties prop;
-//	public static Logger logger = LogManager.getLogger(TestBase.class); //looger to show logs on console 
+	public static Logger logger = LogManager.getLogger(TestBase.class); //logger to show logs on console 
 
 
 /////////////////// properties file key value pair 
@@ -78,7 +78,7 @@ public class TestBase {
 			driver = new ChromeDriver(options);
 			
 //			System.out.println("*******Driver Intilaized*******");
-//			//logger.info("*******ServerDriver Intilaized*******");
+//			logger.info("*******ServerDriver Intilaized*******");
 ///////////////////// Navigate to url and login to marketdojo application 
 			driver.manage().window().maximize();
 	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -88,7 +88,7 @@ public class TestBase {
 			driver.findElement(By.xpath("//input[@name ='commit']")).click();
 			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Dashboard')]")));
-			//logger.info("Logged in and redirect to MD Dashbaord");
+			logger.info("Logged in and redirect to MD Dashbaord");
 			
 		} else if (ExecutionLocation.equals("local")) {
 //			System.setProperty("webdriver.chrome.driver", prop.getProperty("driverpath"));
@@ -96,7 +96,7 @@ public class TestBase {
 ////////////////Adding WebDriverManger 
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			//logger.info("******Local Driver Intilaized*****");
+			logger.info("******Local Driver Intilaized*****");
 ///////////////// Navigate to url and login to marketdojo application 
 			driver.manage().window().maximize();
 			driver.manage().deleteAllCookies();
@@ -107,7 +107,7 @@ public class TestBase {
 			driver.findElement(By.xpath("//input[@name ='commit']")).click();
 //			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 //			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Dashboard')]")));
-			//logger.info("Logged in and redirect to MD Dashbaord");// Redirect to MD Dashboard 
+			logger.info("Logged in and redirect to MD Dashbaord");// Redirect to MD Dashboard 
 		}
 	}
 	
